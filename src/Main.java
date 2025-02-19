3851ff9ef93dc9419c46795c3f6c2b3782a39374
Jonathan Tromp <jonatromp45@gmail.com>
2025-02-19 12:17:28 +0000

    Completed simple rock paper scissors game, functionality to repeat game until user decides to stop.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+            Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String[] rps = {"Rock", "Paper", "Scissors"};
+            String computerMove = rps[new Random().nextInt(rps.length)];
+
+            String playerMove;
+
+            while (true) {
+                System.out.println("Please enter your move (rock, paper or scissors)");
+                playerMove = scanner.nextLine();
+                if (playerMove.equalsIgnoreCase("rock") || playerMove.equalsIgnoreCase("paper") || playerMove.equalsIgnoreCase("scissors")) {
+                    break;
+                }
+                System.out.println(playerMove + " is not valid");
+            }
+
+            System.out.println("Computer move is: " + computerMove);
+
+            if (playerMove.equalsIgnoreCase(computerMove)) {
+                System.out.println("Game was a tie");
+            } else if (playerMove.equalsIgnoreCase("rock")) {
+                if (computerMove.equalsIgnoreCase("paper")) {
+                    System.out.println("You lose");
+                } else if (computerMove.equalsIgnoreCase("scissors")) {
+                    System.out.println("You win");
+                }
+            } else if (playerMove.equalsIgnoreCase("paper")) {
+                if (computerMove.equalsIgnoreCase("rock")) {
+                    System.out.println("You win");
+                } else if (computerMove.equalsIgnoreCase("scissors")) {
+                    System.out.println("You lose");
+                }
+            } else if (playerMove.equalsIgnoreCase("scissors")) {
+                if (computerMove.equalsIgnoreCase("paper")) {
+                    System.out.println("You win");
+                } else if (computerMove.equalsIgnoreCase("rock")) {
+                    System.out.println("You lose");
+                }
+            }
+
+            System.out.println("Play again? (Y or N)");
+            String playAgain = scanner.nextLine();
+
+            if (!playAgain.equalsIgnoreCase("Y")) {
+                System.out.println("Game over");
+                break;
+            }
+        }
     }
 }
